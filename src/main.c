ec2444eb1015968ac6fcbe78c8f76919f0e3dd9e
UNSTOP4BLE <bruno.juraski1@gmail.com>
2023-06-30 00:23:08 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -37,6 +37,48 @@ void ErrorLock(void)
     }
 }
 
+void Character_shitFromFile(Character *this, const char *path, int offstart)
+{
+    int offset = offstart;
+
+    if (offstart != 0)
+        this->file = IO_Read(path);
+    CharacterFileHeader *tmphdr = (CharacterFileHeader *)this->file;
+    offset += sizeof(CharacterFileHeader);
+    this->animatable.anims = (const Animation *)(offset + this->file); 
+    offset += sizeof(Animation) * tmphdr->size_animation;
+    printf("%d size\n", sizeof(Animation));
+    this->frames = (const CharFrame *)(offset + this->file);
+
+/*
+    printf("struct %d, \n", tmphdr->size_struct);
+    printf("frames %d, \n", tmphdr->size_frames);
+    printf("animation %d, \n", tmphdr->size_animation);
+    
+    printf("scripts %d %d %d %d %d %d %d %d %d, \n", tmphdr->sizes_scripts[0], tmphdr->sizes_scripts[1], tmphdr->sizes_scripts[2], tmphdr->sizes_scripts[3], tmphdr->sizes_scripts[4], tmphdr->sizes_scripts[5], tmphdr->sizes_scripts[6], tmphdr->sizes_scripts[7], tmphdr->sizes_scripts[8]);
+
+    printf("spec %d, \n", tmphdr->spec);
+    printf("health %d, \n", tmphdr->health_i);
+    printf("bar %d, \n", (uint32_t)tmphdr->health_bar);
+    printf("focus %d %d %d, \n", tmphdr->focus_x, tmphdr->focus_y, tmphdr->focus_zoom);
+
+        */
+
+    for (int i = 0; i < tmphdr->size_animation; i++) {
+        printf("sped %d frames", this->animatable.anims[i].spd);
+        for (int i2 = 0; i2 < tmphdr->sizes_scripts[i]; i2 ++)
+            printf(" %d ", this->animatable.anims->script[i2]);
+
+        printf("\n");
+    }
+
+        for (int i = 0; i < tmphdr->size_frames; ++i)
+        {
+            printf("tex %d, frames %d %d %d %d offsets %d %d\n", (unsigned int)this->frames[i].tex, this->frames[i].src[0], this->frames[i].src[1], this->frames[i].src[2], this->frames[i].src[3], this->frames[i].off[0], this->frames[i].off[1] ); 
+        }   
+
+}
+int offstarts;
 //Entry point                                                                             
 int main(int argc, char **argv)                                                                                                                                                        
 {
@@ -69,8 +111,6 @@ int main(int argc, char **argv)
   //  Menu_Load(MenuPage_Opening);
 
     Character testchar;
-    Character_FromFile(&testchar, "\\CHAR\\DAD.CHR;1");
-
     //Game loop
     while (PSX_Running()) {
 #ifndef NDEBUG
@@ -83,9 +123,25 @@ int main(int argc, char **argv)
 //        Audio_FeedStream();
         Pad_Update();
 
+        if (testchar.animatable.anims[0].spd == 2)
+        FntPrint(-1, "YAY OFFSET %d\n", offstarts);
+        else
+            pad_state.press |= PAD_UP;
+        FntPrint(-1, "OFFSET %d", offstarts);
+            if (pad_state.press & PAD_UP)
+            {
+                offstarts ++;
+                Character_shitFromFile(&testchar, "\\CHAR\\DAD.CHR;1", offstarts);
+            }
+            if (pad_state.press & PAD_DOWN)
+            {
+                offstarts --;
+                Character_shitFromFile(&testchar, "\\CHAR\\DAD.CHR;1", offstarts);
+            }
         //Tick and draw game
         switch (gameloop)
         {
+
          //   case GameLoop_Menu:
           //      Menu_Tick();
            //     break;
